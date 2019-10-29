799645cbef959ee2b92da0b0aaa1c4a220906018
Anton Roslund <enari@minit.nu>
2019-10-29 12:01:25 +0100

    Added tabbar interface

--- Sequor/Views/ContentView.swift
+++ Sequor/Views/ContentView.swift
@@ -1,8 +1,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selection = 1
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            TabView(selection: $selection) {
+                Text("Dashboard View").tabItem {
+                    Image(systemName: "cloud")
+                    Text("Dashboard")
+                }.tag(1)
+                Text("Coupuns View").tabItem {
+                    Image(systemName: "tray.full.fill")
+                    Text("Copouns")
+                }.tag(2)
+                Text("Account View").tabItem {
+                    Image(systemName: "person.circle")
+                    Text("Account")
+                }.tag(3)
+            }
+            .navigationBarTitle("Sequor", displayMode: .inline)
+        }
     }
 }
 
