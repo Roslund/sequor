7aaf52c0ad4fc3e7258a4ff89cd91006371f7abc
Mattia Righetti <matt95.righetti@gmail.com>
2019-11-08 15:39:50 +0100

    Graph animated when you switch to Dashboard

--- Sequor/Views/DashboardView.swift
+++ Sequor/Views/DashboardView.swift
@@ -4,7 +4,7 @@ struct DashboardView: View {
     var body: some View {
         NavigationView {
             
-            CapsuleGraph(value: 29.34, maxvalue: 50.0, textLabel: "CO2 Saved")
+            CapsuleGraph(value: 29.34, maxValue: 50.0, textLabel: "CO2 Saved")
             
             .navigationBarTitle("Seqour CO₂", displayMode: .inline)
         }
