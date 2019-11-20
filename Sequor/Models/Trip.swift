2debfdc1160a20a58fdddc62359e06bc37931051
Anton Roslund <enari@minit.nu>
2019-11-20 13:42:15 +0100

    LocationLogging Test

--- Sequor/Models/Trip.swift
+++ Sequor/Models/Trip.swift
@@ -4,13 +4,23 @@ struct Trip: Codable {
   var startDate: Date = Date()
   var endDate: Date?
 
-  var locations: [Coordinate] = []
+  var locations: [Location] = []
+}
 
+struct Location: Codable {
+  /// The timestam the location was recorded
+  let timestamp: Date
+  /// Speed at location (m/s)
+  let speed: Double
+  /// Course 0-359.9 degrees
+  let course: Double
+  /// The poistion
+  let coordinate: Coordinate
 }
 
 struct Coordinate: Codable {
   ///The latitude in degrees.
-  var latitude: Double
+  let latitude: Double
   ///The longitude in degrees.
-  var longitude: Double
+  let longitude: Double
 }
