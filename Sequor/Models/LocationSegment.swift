094dc3dd31f85fd49a02ab247da27da9fe872ed7
Anton Roslund <enari@minit.nu>
2019-12-11 13:56:03 +0100

    Minimum accuracy & 100% documentation coverage.

--- Sequor/Models/LocationSegment.swift
+++ Sequor/Models/LocationSegment.swift
@@ -3,6 +3,8 @@ import CoreLocation
 
 /// Structure for displaying trip segments on a MKMapView
 struct LocationsSegment {
+    /// The classified user activity.
     let activity: CMMotionActivity.Classification
+    /// All logged cordinates for this segment.
     var coordinates: [CLLocationCoordinate2D] = []
 }
